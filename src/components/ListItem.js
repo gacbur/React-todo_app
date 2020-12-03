b7815cc8f73f30e18ed37506db3d63815509d818
Kacper <kacper0775@gmail.com>
2020-12-03 12:02:27 +0100

    add final version

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import cross from '../icons/cross.svg'
 import { GlobalStateContext } from '../context/GlobalStateContext'
 
